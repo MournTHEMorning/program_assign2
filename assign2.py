c88a9f36d3a881a2e0212de8cc5a66369a571275
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-10-24 20:21:49 -0400

    Oct 24: Product class made

--- assign2.py
+++ assign2.py
@@ -1,1 +1,6 @@
+import product
 #assignment 2 created
+
+#    def __init__(self, product_code, product_name, product_price, product_cost, product_stock, product_monthUnit):
+
+item=product.Product("ahinw", "oranges", 4.23,0.69,72,12345)
