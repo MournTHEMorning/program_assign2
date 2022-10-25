70ba6b8b7991bbe34a3dd9aba1f4dd665f232703
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-10-24 22:28:26 -0400

    Oct 24: getter methods commented out

--- product.py
+++ product.py
@@ -7,20 +7,20 @@ class Product:
         self.cost=product_cost
         self.monthly_unit=product_monthUnit
     
-    def getCode(self):
-        return self.code
+    # def getCode(self):
+    #     return self.code
 
-    def getName(self):
-        return self.name
+    # def getName(self):
+    #     return self.name
 
-    def getPrice(self):
-        return self.price
+    # def getPrice(self):
+    #     return self.price
 
-    def getCost(self):
-        return self.cost
+    # def getCost(self):
+    #     return self.cost
 
-    def getStock(self):
-        return self.stock
+    # def getStock(self):
+    #     return self.stock
 
-    def getMonthly_Unit(self):
-        return self.monthly_unit
+    # def getMonthly_Unit(self):
+    #     return self.monthly_unit
