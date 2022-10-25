ecda2363c55cf48550d73bf61c03e424e8a75567
Mourn <82979811+MournTHEMorning@users.noreply.github.com>
2022-10-24 22:16:38 -0400

    Oct 24: Stock Statement layout, randomizer and Month report begun

--- assign2.py
+++ assign2.py
@@ -1,7 +1,36 @@
-import product
+import product, random
 #assignment 2 created
 
-#    def __init__(self, product_code, product_name, product_price, product_cost, product_stock, product_monthUnit):
+def simulate(rand):
+    #effect MEASURES HOW rand VARIABLE EFFECTS THE MONTH
+    effect=random.randint(0,1)
 
-item=product.Product("ahinw", "oranges", 4.23,0.69,72,12345)
-print(item.getCode(), item.getName(), item.getPrice(),item.getCost(), item.getStock(),item.getMonthly_Unit())
+    #returns a positive value
+    if(effect):
+        return rand
+    
+    #returns a negative value, which is the complement of the original rand
+    else:
+        rand-=10
+        return rand
+
+
+#player assigning values
+print("Welcome to Programming Principles Product Inventory")
+userCode=input("Please enter the Product Code: ")
+userName=input("Please enter the Product Name: ")
+userStock=int(input("Please enter the Current Stock: "))
+userPrice=float(input("Please enter the Product Sale Price: "))
+userCost=float(input("Please enter the Product Manufacture Cost: "))
+userMonthlyProduction=float(input("Please enter estimated monthly production: "))
+
+
+item=product.Product(userCode,userName,userStock,userPrice,userCost,userMonthlyProduction)
+#print(item.getCode(), item.getName(), item.getStock(),item.getPrice(),item.getCost(), item.getMonthly_Unit())
+
+print(" * "*7,"Programming Principles Stock Statement"," * "*7)
+print("Product Code: {}\nProduct Name: {}\n\nSale Price: {} CAD\nManufacture Cost: {} CAD \nMonthly Production: {} units (Approx.)".format(userCode,userName,userStock,userPrice,userCost,userMonthlyProduction))
+
+simulate(random.randint(0,10))
+
+print("Month ###:\n\tManufactured: {} units\n\tSold: {} units\n\tStock: {} units".format(item.monthly_unit,))
--- product.py
+++ product.py
@@ -1,10 +1,10 @@
 class Product:
-    def __init__(self, product_code, product_name, product_price, product_cost, product_stock, product_monthUnit):
+    def __init__(self, product_code, product_name,product_stock, product_price, product_cost, product_monthUnit):
         self.code=product_code
         self.name=product_name
+        self.stock=product_stock
         self.price=product_price
         self.cost=product_cost
-        self.stock=product_stock
         self.monthly_unit=product_monthUnit
     
     def getCode(self):
